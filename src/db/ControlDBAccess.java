47c21620aa59c56e0b959935b6889d83d54e8b7d
itkaikei <itkaikei@172.17.0.89>
2022-11-08 15:12:52 +0900

    update

--- src/db/ControlDBAccess.java
+++ src/db/ControlDBAccess.java
@@ -1,10 +1,52 @@
 package db;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-class ControlDBAccess {
+public class ControlDBAccess {
 
-	public static void main(String[] args) {
-		// TODO 自動生成されたメソッド・スタブ
+    public static void main(String[] args) {
 
-	}
+    // 変数の準備
+        Connection con = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+
+    // SQL文の作成
+        String sql = "SELECT * FROM test";
+
+        try {
+            // JDBCドライバのロード
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            // データベース接続
+            con = DriverManager.getConnection("jdbc:mysql://localhost:65534/test_db?serverTimezone=JST", "root", "root");
+            // SQL実行準備
+            stmt = con.prepareStatement(sql);
+            // 実行結果取得
+            rs = stmt.executeQuery();
+
+      // データがなくなるまで(rs.next()がfalseになるまで)繰り返す
+            while (rs.next()) {
+                String id = rs.getString("id");
+                String name = rs.getString("name");
+
+                System.out.println(id + ":" + name);
+            }
+        } catch (ClassNotFoundException e) {
+            System.out.println("JDBCドライバのロードでエラーが発生しました");
+        } catch (SQLException e) {
+            System.out.println("データベースへのアクセスでエラーが発生しました。1");
+        } finally {
+            try {
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException e) {
+                System.out.println("データベースへのアクセスでエラーが発生しました。");
+            }
+        }
+    }
 
 }
