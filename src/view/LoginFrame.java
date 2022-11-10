2878b620c026f239f0a1a55e091b0677d539997d
内藤　将太 <itkaikei@172.17.0.88>
2022-11-10 09:50:51 +0900

    追加　LoginFrame

---
+++ src/view/LoginFrame.java
@@ -0,0 +1,109 @@
+package view;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
+public class LoginFrame extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					LoginFrame frame = new LoginFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public LoginFrame() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 757, 674);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		ImageIcon icon = new ImageIcon(LoginFrame.class.getResource("white_00032.jpg"));
+		
+		JPanel panel1 = new JPanel();
+		contentPane.add(panel1);
+		//FlowLayout flowLayout = (FlowLayout) panel1.getLayout();
+		panel1.setBackground(new Color(224, 255, 255));
+		panel1.setBounds(154,100,411,336);
+		panel1.setLayout(null);
+		
+		JLabel lblNewLabel_1 = new JLabel("ログイン");
+		lblNewLabel_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 25));
+		lblNewLabel_1.setBounds(171, 43, 134, 50);
+		panel1.add(lblNewLabel_1);
+		
+		textField = new JTextField();
+		textField.setBounds(127, 103, 168, 19);
+		panel1.add(textField);
+		textField.setColumns(10);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(127, 144, 168, 19);
+		panel1.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblNewLabel_2 = new JLabel("ログインID");
+		lblNewLabel_2.setBounds(57, 106, 68, 13);
+		panel1.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("パスワード");
+		lblNewLabel_3.setBounds(57, 147, 68, 13);
+		panel1.add(lblNewLabel_3);
+		
+		JButton btnNewButton = new JButton("ログイン");
+		btnNewButton.setEnabled(false);
+		btnNewButton.setForeground(new Color(255, 255, 255));
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBackground(new Color(0,0,0));
+		btnNewButton.setFont(new Font("MS UI Gothic", Font.PLAIN, 14));
+		btnNewButton.setBounds(145, 205, 115, 33);
+		panel1.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("キャンセル");
+		btnNewButton_1.setBackground(Color.BLACK);
+		btnNewButton_1.setEnabled(false);
+		btnNewButton_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 14));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton_1.setBounds(145, 252, 115, 33);
+		panel1.add(btnNewButton_1);
+		JLabel Label1 = new JLabel(new ImageIcon(LoginFrame.class.getResource("/image/white_00032.jpg")));
+		Label1.setBounds(0, 0, 740, 635);
+		contentPane.add(Label1);
+		Label1.setLayout(null);
+		}
+}
