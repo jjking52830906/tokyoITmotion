49a32a651b78952f66067c5ec48aef5bbb077d93
根元　台地 <itkaikei@172.17.0.90>
2022-11-11 11:12:56 +0900

    update

--- src/view/Jjsearch.java
+++ src/view/Jjsearch.java
@@ -51,7 +51,7 @@ public class Jjsearch extends JFrame {
 		contentPane.add(lblNewLabel_1);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setBounds(244, 41, 37, 21);
+		comboBox.setBounds(244, 41, 39, 21);
 		comboBox.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"}));
 		contentPane.add(comboBox);
 		
@@ -60,7 +60,7 @@ public class Jjsearch extends JFrame {
 		contentPane.add(lblNewLabel_2);
 		
 		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setBounds(321, 41, 37, 21);
+		comboBox_1.setBounds(321, 41, 39, 21);
 		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"}));
 		contentPane.add(comboBox_1);
 		
