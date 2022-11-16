f6268f681aa904378d0ffa9a19fed4f30d91a20b
佐藤　潤弥 <itkaikei@172.17.64.51>
2022-11-16 13:29:15 +0900

    ちょっとした変更

--- src/db/AccountRegisterDBAccess.java
+++ src/db/AccountRegisterDBAccess.java
@@ -1,12 +1,31 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class AccountRegisterDBAccess extends ControlDBAccess{
 	public int registerAccount(String id, String pass, String fname, String lname, String year, String month, String day, String radio, String fAddress, String lAddress, String tel) throws Exception{
 		int result = 0;
 		Connection con = createConnection();
 		PreparedStatement pstmt = null;
 		try {
-			String sql = ""
+			String sql = "INSERT INTO CUSTOMER";
+			pstmt.setString(1, id);
+			pstmt.setString(2, sql);
+			result = pstmt.executeUpdate();
+		}catch(SQLException e) {
+			throw new Exception("アカウント登録処理に失敗しました。");
+		}finally {
+			try {
+				if(pstmt != null) {
+					pstmt.close();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
 		}
+		closeConnection(con);
+		return result;
 	}
 }
