b79c8f5599375555e3c065fa3919be1878047c4d
佐藤　潤弥 <itkaikei@172.17.64.51>
2022-11-30 12:08:46 +0900

    aaaa

--- src/db/AccountRegisterDBAccess.java
+++ src/db/AccountRegisterDBAccess.java
@@ -1,7 +1,6 @@
 package db;
 
 import java.sql.Connection;
-<<<<<<< HEAD
 import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
@@ -24,45 +23,6 @@ public class AccountRegisterDBAccess extends ControlDBAccess{
 			pstmt.setString(8, lAddress);
 			pstmt.setString(9, tel);
 			result = pstmt.executeUpdate();
-=======
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
-
-public class AccountRegisterDBAccess extends ControlDBAccess{
-	public int registerAccount(String firstname, String lastname, String birth, String sex, String firstaddress, String lastaddress, String tel) throws Exception{
-	
-		int result = 0;
-		Connection con = createConnection();
-		PreparedStatement pstmt = null;
-		try {
-			String sql = "INSERT INTO CUSTOMER(FIRSTNAME,LASTNAME,BIRTH,,SEX,FIRSTADDRESS,LASTADDRESS,TEL) VALUES(?,?,?,?,?,?,?);";
-			
-			pstmt = con.prepareStatement(sql);
-			
-			String fn = firstname;
-			pstmt.setString(1, fn);
-			
-			String ln = lastname;
-			pstmt.setString(2, ln);
-			
-			String birthday = birth;
-			pstmt.setString(3, birthday);
-			
-			String sexx = sex;
-			pstmt.setString(4, sexx);
-			
-			String fa = firstaddress;
-			pstmt.setString(5, fa);
-			
-			String la = lastaddress;
-			pstmt.setString(6, la);
-			
-			int tell = Integer.parseInt(tel);
-			pstmt.setInt(7, tell);
-	
-			pstmt.executeUpdate();
-		
->>>>>>> branch 'master' of https://github.com/jjking52830906/tokyoITmotion.git
 		}catch(SQLException e) {
 			throw new Exception("アカウント登録処理に失敗しました。!");
 		}finally {
