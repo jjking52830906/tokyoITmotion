d78d84259a2ef4dca9ef8e464f7cff43868f9414
佐藤　潤弥 <h270p@DESKTOP-2P5L4K5>
2022-12-05 13:05:00 +0900

    あ

--- src/db/LoginNinshouDBAccess.java
+++ src/db/LoginNinshouDBAccess.java
@@ -1,6 +1,43 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import model.Customer;
+
 public class LoginNinshouDBAccess extends ControlDBAccess{
 	
-	public ArrayList<Customer>
+	public ArrayList<Customer> loginNinshou(String id, String pass)throws Exception{
+		Connection con = createConnection();
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		ArrayList<Customer> list = new ArrayList<Customer>();
+		try {
+			if(con != null) {
+				String sql = "SELECT ROGID, ROGPASS FROM CUSTOMER WHERE ROGID = ? AND ROGPASS = ?;";
+				pstmt = con.prepareStatement(sql);
+				pstmt.setString(1, id);
+				pstmt.setString(2, pass);
+				rs = pstmt.executeQuery();
+				while(rs.next() == true) {
+					
+				}
+			}
+		}catch (SQLException e) {
+			throw new Exception("IDとパスワード情報取得処理に失敗しました。");
+		}finally {
+			try {
+				if(rs != null) {
+					pstmt.close();
+				}
+			}catch(SQLException e) {
+				e.printStackTrace();
+			}
+		}
+		closeConnection(con);
+		return list;
+	}
 }
