17ac686d71f9b63338b33d2038da026eb87a9cbf
加藤 　唯 <itkaikei@172.17.0.87>
2022-12-08 10:00:53 +0900

    add 追加

--- src/action/YoyakuRegisterAction.java
+++ src/action/YoyakuRegisterAction.java
@@ -9,7 +9,7 @@ import db.YoyakuRegisterDBAccess;
 public class YoyakuRegisterAction {
     public int execute(int insid, Date resdate, Time strtime, Time fintime, int custid, String purpose, String place) throws Exception {
         YoyakuRegisterDBAccess crdb = new YoyakuRegisterDBAccess();
-        int res = crdb.registerYoyaku(insid, resdate, strtime, fintime, custid, purpose, place);
-        return res;
+        int rem = crdb.registerYoyaku(insid, resdate, strtime, fintime, custid, purpose, place);
+        return rem;
     }
 }
