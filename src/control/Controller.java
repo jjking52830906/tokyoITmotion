845c8d9d474075f144d1d51235d34290350693ae
加藤 　唯 <itkaikei@172.30.3.9>
2022-12-13 09:59:31 +0900

    add コントローラーの追加

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.sql.Date;
+import java.sql.Time;
 
 import action.AccountRegisterAction;
 import action.AkiSearchDisplayAction;
@@ -14,6 +15,7 @@ import action.LoginafterDisplayAction;
 import action.MainMenuDisplayAction;
 import action.RegisterDisplayAction;
 import action.YoyakuDisplayAction;
+import action.YoyakuRegisterAction;
 import action.YoyakukauninDisplayAction;
 
 public class Controller {
@@ -52,6 +54,12 @@ public class Controller {
 		AccountRegisterAction action = new AccountRegisterAction();
 		int res = action.execute(fname, lname, date, radio, fAddress, lAddress, tel);
 		return res;
+	}
+	
+	public static int yoyokuRegister(int insid, Date resdate, Time strtime, Time fintime, int custid, String purpose, String place) throws Exception{
+		YoyakuRegisterAction action = new YoyakuRegisterAction();
+		int rem = action.execute(insid, resdate, strtime, fintime, custid, purpose, place);
+		return rem;
 	}
 	
 	public static int loginRegister(String id, String pass)throws Exception{
