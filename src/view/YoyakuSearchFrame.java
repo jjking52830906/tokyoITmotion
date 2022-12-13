1477b4b238ccb3831abbe26c8ffc1d5baacb5b15
佐藤　潤弥 <itkaikei@172.17.0.37>
2022-12-13 13:40:04 +0900

    aa

--- src/view/YoyakuSearchFrame.java
+++ src/view/YoyakuSearchFrame.java
@@ -2,7 +2,7 @@ package view;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Calendar;
+import java.sql.Date;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
@@ -18,14 +18,26 @@ import control.Controller;
 public class YoyakuSearchFrame extends JFrame implements ActionListener{
 
 	private JPanel contentPane;
-	private JButton Return;
-	private JComboBox cbbefore;
-	private JComboBox cbafter;
-	private JButton Search;
+	
+	private JLabel lblDate;
+	private JComboBox cbYear;
+	private JLabel lblYear;
 	private JComboBox cbMonth;
 	private JLabel lblMonth;
 	private JComboBox cbDay;
 	private JLabel lblDay;
+	
+	private JLabel lblPM;
+	private JComboBox cbbefore;
+	private JLabel lblBefore;
+	private JComboBox cbafter;
+	private JLabel lblAfter;
+	
+	private JLabel lblInstitution;
+	private JComboBox cbInstitution;
+	
+	private JButton Search;
+	private JButton Return;
 
 	/**
 	 * Create the frame.
@@ -33,182 +45,95 @@ public class YoyakuSearchFrame extends JFrame implements ActionListener{
 	public YoyakuSearchFrame() {
 		setTitle("【予約登録画面】");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(450, 200, 448, 393);
+		setBounds(450, 200, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-
+		
+		lblDate = new JLabel("年月日");
+		lblDate.setBounds(25, 45, 50, 13);
+		contentPane.add(lblDate);
+		
+		cbYear = new JComboBox();
+		cbYear.setBounds(98, 41, 96, 21);
+		cbYear.setModel(new DefaultComboBoxModel(new String[] {"2022", "2023"}));
+		contentPane.add(cbYear);
+		
+		lblYear = new JLabel("年");
+		lblYear.setBounds(202, 45, 21, 13);
+		contentPane.add(lblYear);
+		
+		String[] Month={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
+		cbMonth = new JComboBox(Month);
+		cbMonth.setBounds(244, 41, 39, 21);
+		getContentPane().add(cbMonth);
+		cbMonth.addActionListener(this);
+		
+		lblMonth = new JLabel("月");
+		lblMonth.setBounds(288, 45, 21, 13);
+		contentPane.add(lblMonth);
+		
+		String[] Day={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
+		cbDay = new JComboBox(Day);
+		cbDay.setBounds(321, 41, 39, 21);
+		getContentPane().add(cbDay);
+		cbDay.addActionListener(this);
+		
+		lblDay = new JLabel("日");
+		lblDay.setBounds(370, 45, 21, 13);
+		contentPane.add(lblDay);
+		
+		lblPM = new JLabel("時刻");
+		lblPM.setBounds(38, 108, 37, 13);
+		contentPane.add(lblPM);
+		
+		String[] before={"9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
+		cbbefore = new JComboBox(before);
+		cbbefore.setBounds(98, 104, 39, 21);
+		getContentPane().add(cbbefore);
+		cbbefore.addActionListener(this);
+		
+		lblBefore = new JLabel("時");
+		lblBefore.setBounds(149, 108, 21, 13);
+		contentPane.add(lblBefore);
+		
+		JLabel lblNewLabel = new JLabel("～");
+		lblNewLabel.setBounds(193, 108, 30, 13);
+		contentPane.add(lblNewLabel);
+		
+		String[] after={"9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
+		cbafter = new JComboBox(after);
+		cbafter.setBounds(244, 104, 39, 21);
+		getContentPane().add(cbafter);
+		cbafter.addActionListener(this);
+		
+		lblAfter = new JLabel("時");
+		lblAfter.setBounds(293, 108, 21, 13);
+		contentPane.add(lblAfter);
+		
+		lblInstitution = new JLabel("施設");
+		lblInstitution.setBounds(38, 163, 30, 13);
+		contentPane.add(lblInstitution);
+		
+		String[] institution = {"体育館", "野球場", "サッカー場", "テニスコート", "プール"};
+		cbInstitution = new JComboBox(institution);
+		cbInstitution.setBounds(98, 159, 114, 21);
+		getContentPane().add(cbInstitution);
+		cbInstitution.addActionListener(this);
+		
 		Search = new JButton("検索");
-		Search.setBounds(128, 277, 86, 39);
+		Search.setBounds(98, 218, 91, 21);
 		contentPane.add(Search);
+		getContentPane().add(Search);
 		Search.addActionListener(this);
-
-		Return = new JButton("戻る");
-		Return.setBounds(280, 277, 91, 40);
+		
+		Return = new JButton("キャンセル");
+		Return.setBounds(244, 218, 104, 21);
 		contentPane.add(Return);
 		getContentPane().add(Return);
 		Return.addActionListener(this);
-
-		JLabel lblNewLabel = new JLabel("年月日");
-		lblNewLabel.setBounds(47, 60, 50, 13);
-		contentPane.add(lblNewLabel);
-
-		JLabel lblNewLabel_1 = new JLabel("時刻");
-		lblNewLabel_1.setBounds(47, 134, 50, 13);
-		contentPane.add(lblNewLabel_1);
-
-		JLabel lblNewLabel_2 = new JLabel("施設");
-		lblNewLabel_2.setBounds(47, 208, 50, 13);
-		contentPane.add(lblNewLabel_2);
-
-		Calendar cl = Calendar.getInstance();
-		int ye = cl.get(Calendar.YEAR);
-		int ye1 = ye++;
-		Integer yex = Integer.valueOf(ye);
-		Integer yex1 = Integer.valueOf(ye1);
-		String yez = yex.toString();
-		String yez1 = yex1.toString();
-
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {yez1, yez}));
-		comboBox.setBounds(128, 55, 66, 21);
-		contentPane.add(comboBox);
-
-		int d = cl.get(Calendar.MONTH);
-		int dd = d + 1;
-		int ddd = d + 1;
-
-
-		String M[];
-		M = new String[12];
-
-		for(int count=0;count<12;count++) {
-
-			Integer dx = Integer.valueOf(dd);
-			String d2 = dx.toString();
-
-			M[count]=d2;
-
-			dd++;
-			if(dd==13) {
-				dd=1;
-			}
-		}
-
-		String[] Month={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12"};
-		cbMonth = new JComboBox(M);
-		cbMonth.setBounds(234, 56, 39, 21);
-		getContentPane().add(cbMonth);
-		cbMonth.addActionListener(this);
-
-		int dayy = cl.get(Calendar.DATE);
-		int day1 = dayy;
-
-		String D[];
-		D = new String[31];
-
-		if(ddd==2) {
-			for(int count=0;count<28;count++) {
-
-				Integer dda = Integer.valueOf(day1 );
-				String dax = dda.toString();
-
-				D[count]=dax;
-
-				day1++;
-				if(day1==29) {
-					day1=1;
-				}
-			}
-		}
 		
-		if(ddd==4 || ddd==6 || ddd==9 || ddd==11) {
-			for(int count=0;count<29;count++) {
-
-				Integer dda = Integer.valueOf(day1);
-				String dax = dda.toString();
-
-				D[count]=dax;
-
-				day1++;
-				if(day1==30) {
-					day1=1;
-				}
-			}
-		}
-
-		if(ddd==1 || ddd==3 || ddd==5 || ddd==7 || ddd==8 || ddd==10 || ddd==12) {
-			for(int count=0;count<30;count++) {
-
-				Integer dda = Integer.valueOf(day1);
-				String dax = dda.toString();
-
-				D[count]=dax;
-
-				day1++;
-				if(day1==31) {
-					day1=1;
-				}
-			}
-		}
-
-
-
-		String[] Day={"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31"};
-		cbDay = new JComboBox(D);
-		cbDay.setBounds(321, 56, 39, 21);
-		getContentPane().add(cbDay);
-		cbDay.addActionListener(this);
-
-
-		JLabel lblNewLabel_3 = new JLabel("月");
-		lblNewLabel_3.setBounds(288, 60, 18, 13);
-		contentPane.add(lblNewLabel_3);
-
-		JLabel lblNewLabel_4 = new JLabel("日");
-		lblNewLabel_4.setBounds(372, 60, 50, 13);
-		contentPane.add(lblNewLabel_4);
-
-		JLabel lblNewLabel_5 = new JLabel("年");
-		lblNewLabel_5.setBounds(204, 60, 18, 13);
-		contentPane.add(lblNewLabel_5);
-
-		String[] before={"9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
-		cbbefore = new JComboBox(before);
-		cbbefore.setBounds(128, 130, 42, 21);
-		getContentPane().add(cbbefore);
-		cbbefore.addActionListener(this);
-
-
-		JLabel lblNewLabel_6 = new JLabel("時");
-		lblNewLabel_6.setBounds(179, 134, 18, 13);
-		contentPane.add(lblNewLabel_6);
-
-		JLabel lblNewLabel_7 = new JLabel("～");
-		lblNewLabel_7.setBounds(222, 134, 24, 13);
-		contentPane.add(lblNewLabel_7);
-
-		String[] after={"9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21"};
-		cbafter = new JComboBox(after);
-		cbafter.setBounds(264, 130, 42, 21);
-		getContentPane().add(cbafter);
-		cbafter.addActionListener(this);
-
-		JLabel lblNewLabel_8 = new JLabel("時");
-		lblNewLabel_8.setBounds(325, 134, 21, 13);
-		contentPane.add(lblNewLabel_8);
-
-		JComboBox comboBox_5 = new JComboBox();
-		comboBox_5.setModel(new DefaultComboBoxModel(new String[] {"体育館", "野球場", "サッカー場", "テニスコート", "プール"}));
-		comboBox_5.setBounds(128, 204, 109, 21);
-		contentPane.add(comboBox_5);
-
-		JLabel lblNewLabel_9 = new JLabel("※予約可能日は本日から1年後まで");
-		lblNewLabel_9.setBounds(131, 83, 215, 13);
-		contentPane.add(lblNewLabel_9);
-
 		setVisible(true);
 	}
 
@@ -216,12 +141,17 @@ public class YoyakuSearchFrame extends JFrame implements ActionListener{
 		if(e.getSource()==Search) {
 			String before =(String)cbbefore.getSelectedItem();
 			String after = (String)cbafter.getSelectedItem();
+			String year = (String)cbYear.getSelectedItem();
 			String month = (String)cbMonth.getSelectedItem();
 			String day = (String)cbDay.getSelectedItem();
+			String basho = (String)cbInstitution.getSelectedItem();
 			int benum = Integer.parseInt(before);
 			int afnum = Integer.parseInt(after);
 			int bemonth = Integer.parseInt(month);
 			int beday = Integer.parseInt(day);
+			String strDate = year + "-" + month + "-" + day;
+			Date date = Date.valueOf(strDate);
+			
 
 			if(benum>=afnum&&bemonth==2&&beday>28) {
 				JOptionPane.showMessageDialog(this, "正しい日付と時刻を入力してください。", "エラー", JOptionPane.WARNING_MESSAGE);
@@ -245,6 +175,12 @@ public class YoyakuSearchFrame extends JFrame implements ActionListener{
 				JOptionPane.showMessageDialog(this, "正しい日付を入力してください。", "エラー", JOptionPane.WARNING_MESSAGE);
 			}else if(bemonth==11&&beday==31) {
 				JOptionPane.showMessageDialog(this, "正しい日付を入力してください。", "エラー", JOptionPane.WARNING_MESSAGE);
+			}
+			try {
+				String[] data = {before, after, basho};
+				String[] tableData = Controller.AkiSearch(date, data);
+			}catch (Exception ex) {
+				
 			}
 		}
 		if(e.getSource() == Return) {
--- src/view/Yoyakukensakukekka.java
+++ src/view/Yoyakukensakukekka.java
@@ -1,38 +1,31 @@
 package view;
 
-import java.awt.Color;
-import java.awt.EventQueue;
-import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
-import javax.swing.border.LineBorder;
+import javax.swing.table.DefaultTableColumnModel;
 import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableColumn;
 
-public class Yoyakukensakukekka extends JFrame {
+import control.Controller;
+
+public class Yoyakukensakukekka extends JFrame implements ActionListener {
 
 	private JPanel contentPane;
-	private JTable table;
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Yoyakukensakukekka frame = new Yoyakukensakukekka();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+	private JScrollPane scrollPane;
+	private DefaultTableModel tableModel;
+	private JTable table;
+	
+	private JButton Return;
 
 	/**
 	 * Create the frame.
@@ -40,55 +33,59 @@ public class Yoyakukensakukekka extends JFrame {
 	public Yoyakukensakukekka() {
 		setTitle("【予約検索結果画面】");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 517, 438);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		table = new JTable();
-		table.setBorder(new LineBorder(new Color(0, 0, 0)));
-		table.setModel(new DefaultTableModel(
-			new Object[][] {
-				{"", null, null},
-				{null, "", null},
-				{null, null, ""},
-				{null, null, null},
-				{null, null, ""},
-				{null, null, null},
-				{null, null, null},
-			},
-			new String[] {
-				"\u65BD\u8A2D\u540D", "\u5E74\u6708\u65E5", "\u6642\u523B"
-			}
-		));
-		table.getColumnModel().getColumn(0).setPreferredWidth(80);
-		table.getColumnModel().getColumn(0).setMinWidth(29);
-		table.getColumnModel().getColumn(1).setPreferredWidth(80);
-		table.getColumnModel().getColumn(1).setMinWidth(23);
-		table.getColumnModel().getColumn(2).setPreferredWidth(83);
-		table.getColumnModel().getColumn(2).setMinWidth(23);
-		table.setBounds(30, 83, 379, 112);
-		contentPane.add(table);
+		Return = new JButton("キャンセル");
+		Return.setBounds(367, 358, 128, 31);
+		contentPane.add(Return);
+		getContentPane().add(Return);
+		Return.addActionListener(this);
 		
-		JButton btnNewButton = new JButton("戻る");
-		btnNewButton.setBounds(303, 218, 88, 33);
-		contentPane.add(btnNewButton);
+		scrollPane = new JScrollPane();
+		scrollPane.setToolTipText("");
+		scrollPane.setBounds(12, 10, 483, 338);
+		contentPane.add(scrollPane);
 		
-		JLabel lblNewLabel = new JLabel("施設名");
-		lblNewLabel.setFont(new Font("MS UI Gothic", Font.PLAIN, 15));
-		lblNewLabel.setBounds(67, 57, 50, 16);
-		contentPane.add(lblNewLabel);
+		String[] columnNames = {"年月日", "開始時刻", "終了時刻", "施設名"};
+		tableModel = new DefaultTableModel(columnNames, 0);
+		table = new JTable(tableModel);
+		table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
 		
-		JLabel lblNewLabel_1 = new JLabel("年月日");
-		lblNewLabel_1.setFont(new Font("MS UI Gothic", Font.PLAIN, 15));
-		lblNewLabel_1.setBounds(191, 57, 50, 16);
-		contentPane.add(lblNewLabel_1);
+		DefaultTableColumnModel columnModel = (DefaultTableColumnModel) table.getColumnModel();
+		TableColumn column0 = columnModel.getColumn(0);
+		TableColumn column1 = columnModel.getColumn(1);
+		TableColumn column2 = columnModel.getColumn(2);
+		TableColumn column3 = columnModel.getColumn(3);
+		column0.setPreferredWidth(150);
+		column1.setPreferredWidth(100);
+		column2.setPreferredWidth(100);
+		column3.setPreferredWidth(130);
 		
-		JLabel lblNewLabel_2 = new JLabel("時刻");
-		lblNewLabel_2.setFont(new Font("MS UI Gothic", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(321, 57, 50, 16);
-		contentPane.add(lblNewLabel_2);
+		table.addMouseListener(new SearchMouseEvent());
+		
+		scrollPane.setViewportView(table);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO 自動生成されたメソッド・スタブ
+		if(e.getSource() == Return) {
+			setVisible(false);
+			
+			Controller.mainMenuDisplay();
+		}
+	}
+	
+	private class SearchMouseEvent extends MouseAdapter{
+		public void mouseClicked(MouseEvent e) {
+			setVisible(false);
+			
+			
+		}
 	}
 }
